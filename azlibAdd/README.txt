bd44192221bfcbf52ab3a9ec6be40a5862d82ccf
Douglas Meredith <douglasm@email.arizona.edu>
2018-02-26 14:45:23 -0700

    Renamed tools

---
+++ azlibAdd/README.txt
@@ -0,0 +1,45 @@
+Prerequisites:
+--------------
+
+This command line tool relies on the gdal npm package (https://www.npmjs.com/package/gdal). At the time of this writing, the gdal package is not compatibile with the latest version of node.js (9.5.0). It works fine the the current LTS release (8.9.4). If you need to install this version, you can use nvm to manage multiple node versions as follows:
+
+	1) curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+	2) nvm --version  (to verify install)
+	3) nvm install 8.9.4
+	4) nvm use 8.9.4
+
+
+
+Installation:
+-------------
+
+At this point, you should be able to run the tool via node:
+	node index.js <arguments - see Usage>
+
+In order to install as a shell command, run:
+	npm install -g
+
+This copies the tool to a location on our path. You can see where with:
+	which azlibAdd
+
+At this point, you should be able to run the tool like any shell command:
+	azlibAdd <arguments - see Usage>
+
+If you want to be able to run the tool as a shell command and also make changes to index.js that manifest in the shell command, you need to symlink the path to our local copy of index.js. npm will do this for you:
+	npm link
+
+
+Usage:
+------
+
+Run the tool as follows:
+	azlibAdd source_directory gdb_schema_name db_name db_user [db_password]
+
+If db_password is not specified, you will be prompted for it.
+
+Usage guidance is also available from the command line by typing:
+	azlibAdd --help
+
+
+
+
---
+++ azlibAdd/index.js
@@ -0,0 +1,166 @@
+#!/usr/bin/env node
+
+//TODO: There's a lot of common code between this tool and the other two. Look into 
+//refactoring into shared modules.
+
+const args = process.argv.slice(2);
+
+if (args[0] === "--help") {
+	console.log("Usage: azlibAdd source_directory gdb_schema_name db_name db_user [db_password]");
+	return;
+}
+
+const pgp = require("pg-promise")({
+	 //Initialization Options
+});
+let db;
+
+let existingTables;
+//Create list of layers in new gdb
+const gdal = require("gdal");
+const dataset = gdal.open(process.cwd() + "/" + args[0] + "/" + args[0] + ".gdb");
+const layers = dataset.layers.map((layer, i) => {
+	return args[1] + '."' + layer.name + '"';
+});
+
+let collectionID;
+let uploadID;
+
+const ogr2ogr = require('ogr2ogr');
+
+// get password sorted
+let promise = new Promise((resolve) => {
+	if (args.length === 4) {
+		const prompt = require('prompt');
+	  	prompt.message = "";
+	  	prompt.delimiter = "";
+		prompt.start();
+		prompt.get([{
+			name: 'postgres password',
+		    hidden: true,
+			replace: '*'
+		}], (err, result) => {
+			resolve(result['postgres password']);
+		});
+	} else {
+		resolve(args[4]);
+	}
+});
+
+
+promise.then((password) => {
+	args[4] = password;
+
+	const cn = 'postgres://' + args[3] + ':' + args[4] + '@localhost:5432/' + args[2];
+	db = pgp(cn);
+
+	//TODO: This filename will need to be mandated by the dir structure definition
+	//TODO: In fact, we'll probably want a format validation module here to go through the whole directory
+	const metadata = require(process.cwd() + "/" + args[0] + "/project.json");
+		
+	//First, create a new projects record for this project
+	//TODO: Will this always be true? Might need to check for existing record first
+	const projectsInsert = 
+		"insert into public.projects (project_name, project_desc) values ($$" +
+		metadata.metadata.idinfo.citation.citeinfo.title + "$$, $$" +
+		metadata.metadata.idinfo.descript.abstract + "$$) returning project_id";
+	//console.log(projectsInsert);
+	return db.one(projectsInsert).catch(error => {throw new Error(error);});
+}).then(data => {
+	console.log("project_id = " + data.project_id);
+
+	//Next, create a new record in the collections table
+	//TODO: Would like to change this table name to "collections"
+	const collectionsInsert = 
+		"insert into public.collections (project_id, azgs_path) values (" +
+		data.project_id + ", $$" +
+		"<path info here>" + "$$) returning collection_id";
+	//console.log(collectionsInsert);
+	return db.one(collectionsInsert).catch(error => {throw new Error(error);});
+}).then(data => {
+	collectionID = data.collection_id;
+	console.log("collection id = " + collectionID);
+
+	const uploadsInsert = 
+		"insert into public.uploads (collection_id, created_at) values ($$" +
+		collectionID + "$$, current_timestamp) returning upload_id";
+	//console.log(uploadsInsert);
+	return db.one(uploadsInsert).catch(error => {throw new Error(error);});
+}).then(data => {
+	uploadID = data.upload_id;
+
+	//Create list of existing tables for comparison to new gdb content
+	return db.any("select table_name from information_schema.tables where table_schema='" +  args[1] + "'")
+	.catch(error => {throw new Error(error);});
+}).then(tables => {
+	existingTables = tables.map((table, i) => {
+		return args[1] + '."' + table.table_name + '"';
+	});
+	//console.log("existing tables"); console.log(existingTables);
+
+	//append new gdb to existing gdb
+	const ogrPromise = new Promise((resolve, reject) => {
+		ogr2ogr(args[0] + "/" + args[0] + ".gdb")
+		.format('PostgreSQL')
+		.options(['-lco', 'GEOMETRY_NAME=geom', '-lco', 'LAUNDER=NO', '-append'])
+		.destination('PG:host=localhost user=' + args[3] + ' password=' + args[4] + ' dbname=' + args[2] + ' schemas=' + args[1])
+		.exec(function(error, data) {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(data);
+			}
+		});
+	});
+	return ogrPromise.catch(error => {throw new Error(error);});
+}).then(data => {
+	return db.none("vacuum analyze").catch(error => {throw new Error(error);});
+}).then(() => {
+	//find any new tables brought in with this gdb
+	const newTables = layers.reduce((acc, layer) => {
+		if (!existingTables.includes(layer)) {
+			acc.push(layer);
+		}
+		return acc;
+	}, []);
+	console.log("new tables"); console.log(newTables);
+
+	//add collection_id column to each new table
+	const newTablePromises = newTables.map(table => {
+		//console.log(table);
+		return db.none('alter table ' + table + ' add column collection_id integer references public.collections (collection_id)')
+		.then(() => {
+			console.log("successfully added collection_id to " + table)
+		})
+		.catch(error => {throw new Error(error);});
+	});               
+	return Promise.all(newTablePromises).catch(error => {throw new Error(error);});
+}).then (() => {
+	//Set collection_id in each record that has a null
+	//TODO: This approach is a little janky
+	const cidPromises = layers.map((layer, i) => {
+		//console.log("layer = " + layer);
+		return db.none("update " + layer + 
+			" set collection_id = " + collectionID +
+			" where collection_id is null")
+		.then(() => {
+			console.log("successfully updated collection_id for " + layer);
+		})
+		.catch(error => {throw new Error(error);});
+	});
+	return Promise.all(cidPromises).catch(error => {throw new Error(error);});
+}).then(() => {
+	console.log(args[0] + ".gdb successfully added");
+	return db.none("update public.uploads set completed_at = current_timestamp where upload_id=" + uploadID)
+	.catch(error => {throw new Error(error);});
+}).then(() => {
+	console.log("successfully completed upload");
+	pgp.end();
+})
+.catch(error => {
+	console.log(error); 
+});
+
+
+
+
