53fe614d9319fb2b16e84b04bc4fcdb592875899
Douglas Meredith <douglasm@email.arizona.edu>
2018-02-28 16:13:38 -0700

    Added schema specifier for good measure

--- azlibConfigPG/schemas/02-documents.sql
+++ azlibConfigPG/schemas/02-documents.sql
@@ -6,7 +6,7 @@ CREATE TYPE doc_type AS ENUM('report','journal','chapter','book','guide','misc',
 --This is the master_table for the documents schema that details the location of documents and what they are associated with
 CREATE TABLE documents.documents (
 	document_id serial PRIMARY KEY,
-	collection_id integer REFERENCES collections(collection_id),
+	collection_id integer REFERENCES public.collections(collection_id),
 	doc_type doc_type,
 	azgs_path text NOT NULL UNIQUE,
 	doc_doi text,
