070f14f15db33c747951bb3d73396e8d2fdabc77
Douglas Meredith <douglasm@email.arizona.edu>
2018-02-28 16:14:26 -0700

    Added schema specifier for good measure

--- azlibConfigPG/schemas/03-images.sql
+++ azlibConfigPG/schemas/03-images.sql
@@ -6,7 +6,7 @@ CREATE TYPE image_type AS ENUM('photo','raster','pdf','other');
 --This is the master_table for the images schema that details the location of images and what they are associated with
 CREATE TABLE images.images (
         image_id serial PRIMARY KEY,
-        collection_id integer REFERENCES collections(collection_id),
+        collection_id integer REFERENCES public.collections(collection_id),
         image_type image_type,
         azgs_path text NOT NULL UNIQUE,
         caption text NOT NULL,
