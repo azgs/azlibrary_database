bfd7c24bba47f25cacd64b356d3a49bf299f5ab7
Douglas Meredith <douglasm@email.arizona.edu>
2018-03-13 16:55:48 -0700

    Using pdf-parse to process pdf files. Content is converted to tsvector on insert

--- azlibAdd/documents.js
+++ azlibAdd/documents.js
@@ -9,5 +9,31 @@ exports.upload = (dir, collectionID, db) => {
 		return Promise.resolve();
 	}
 
-	return require("./metadata").upload(dir, "documents", collectionID, db);
+	//const fs = require('fs');
+	const path = require('path');
+	const listFiles = p => fs.readdirSync(p).filter(f => !fs.statSync(path.join(p, f)).isDirectory());
+	let files = listFiles(dir);
+	console.log("files = "); console.log(files);
+
+	files = files.filter(file => file.split('.')[file.split('.').length-1].toUpperCase() === "PDF"); 
+
+	//Process each file
+	const promises = files.map((file) => {
+		console.log("processing pdf file " + file);
+
+		const pdf = require('pdf-parse');
+		let dataBuffer = fs.readFileSync(dir + "/" + file);
+		return pdf(dataBuffer).then(data => {
+			//console.log(data.text); 
+			return db.none(
+				"insert into documents.documents (collection_id, azgs_path, text_search, restricted) values (" +
+				collectionID + ", " + 
+				"'" + dir + "/" + file + "', " +
+				"to_tsvector($$" + data.text + "$$), " +
+				"'false')"); 
+		});
+	});
+	return Promise.all(promises).then(() => {
+		return require("./metadata").upload(dir, "documents", collectionID, db);
+	});
 };
