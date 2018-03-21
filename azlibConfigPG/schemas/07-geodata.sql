734c5f51a491a4dcfb9ee18667ea16c0f77128ba
Douglas Meredith <douglasm@email.arizona.edu>
2018-03-21 14:12:30 -0700

    Added metadata_id field

--- azlibConfigPG/schemas/07-geodata.sql
+++ azlibConfigPG/schemas/07-geodata.sql
@@ -1,9 +1,19 @@
 CREATE SCHEMA geodata;
 
+CREATE TABLE geodata.metadata
+(
+	metadata_id serial PRIMARY KEY,
+	collection_id integer REFERENCES public.collections(collection_id) not null, 
+	type text references metadata.types(type_name) not null,
+	json_data jsonb not null,
+	metadata_file text not null
+);
+
 CREATE TABLE geodata.legacy
 (
 	legacy_id serial PRIMARY KEY,
 	collection_id integer REFERENCES public.collections(collection_id), 
+    metadata_id integer REFERENCES geodata.metadata(metadata_id),
 	name text,
 	path text,
 	geom geometry
@@ -13,19 +23,12 @@ create table geodata.rasters
 (
 	raster_id serial PRIMARY KEY,
 	collection_id integer REFERENCES public.collections(collection_id),
-	raster bytea,
+    metadata_id integer REFERENCES geodata.metadata(metadata_id),
+	raster raster,
 	srid integer, 
 	tile_size integer
 	
 );
 
-CREATE TABLE geodata.metadata
-(
-	metadata_id serial PRIMARY KEY,
-	collection_id integer REFERENCES public.collections(collection_id) not null, 
-	type text references metadata.types(type_name) not null,
-	json_data jsonb not null,
-	metadata_file text not null
-);
 
 
