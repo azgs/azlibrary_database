079052c0629d474d74973385c5a8359d73713515
Douglas Meredith <douglasm@email.arizona.edu>
2018-07-10 16:46:33 -0700

    For top-level metadata table, insert geom

--- azlibAdd/metadata.js
+++ azlibAdd/metadata.js
@@ -27,6 +27,7 @@ exports.upload = (rootDir, intermediateDir, schemaName, collectionID, db) => {
 	//Process each file
 	const promises = files.map((file) => {
 		const type = file.split('.')[0].split('-')[0].toUpperCase();
+		logger.silly("metadata type = " + type);
 
 		//If type is not recognized, ignore file
 		if (!global.metadataTypes.includes(type)) {
@@ -70,14 +71,35 @@ exports.upload = (rootDir, intermediateDir, schemaName, collectionID, db) => {
 						});
 					}).then((data) => {
 						//console.log("xml data = " + data);
-						const metadataInsert = 
-							"insert into " + schemaName + ".metadata (collection_id, type, json_data, metadata_file) values (" +
-							collectionID + ", $$" + 
-							type + "$$, $$" + 
-							JSON.stringify(data) + "$$, $$" +
-							path.join(intermediateDir, myDir, file) + "$$) returning metadata_id";
-						//console.log(metadataInsert);
-						//return db.none(metadataInsert).catch(error => {throw new Error(error);});
+						logger.silly("json from metadata = " + global.pp(data));	
+						let metadataInsert; 
+						//If not top level metadata file					
+						if (intermediateDir) {
+							metadataInsert = 
+								"insert into " + schemaName + ".metadata (collection_id, type, json_data, metadata_file) values (" +
+								collectionID + ", $$" + 
+								type + "$$, $$" + 
+								JSON.stringify(data) + "$$, $$" +
+								path.join(intermediateDir, myDir, file) + "$$) returning metadata_id";
+						} else {
+							//TODO: I don't like that these are hardcoded. Maybe put in metadata.types?
+							if ("ISO19115" === type.toUpperCase() || "ISO19139" === type.toUpperCase()) {
+								const minX = data['gmd:MD_Metadata']['gmd:identificationInfo'][0]['gmd:MD_DataIdentification'][0]['gmd:extent'][0]['gmd:EX_Extent'][0]['gmd:geographicElement'][0]['gmd:EX_GeographicBoundingBox'][0]['gmd:westBoundLongitude'][0]['gco:Decimal'][0];
+								const maxX = data['gmd:MD_Metadata']['gmd:identificationInfo'][0]['gmd:MD_DataIdentification'][0]['gmd:extent'][0]['gmd:EX_Extent'][0]['gmd:geographicElement'][0]['gmd:EX_GeographicBoundingBox'][0]['gmd:eastBoundLongitude'][0]['gco:Decimal'][0];
+								const minY = data['gmd:MD_Metadata']['gmd:identificationInfo'][0]['gmd:MD_DataIdentification'][0]['gmd:extent'][0]['gmd:EX_Extent'][0]['gmd:geographicElement'][0]['gmd:EX_GeographicBoundingBox'][0]['gmd:southBoundLatitude'][0]['gco:Decimal'][0];
+								const maxY = data['gmd:MD_Metadata']['gmd:identificationInfo'][0]['gmd:MD_DataIdentification'][0]['gmd:extent'][0]['gmd:EX_Extent'][0]['gmd:geographicElement'][0]['gmd:EX_GeographicBoundingBox'][0]['gmd:northBoundLatitude'][0]['gco:Decimal'][0];
+								logger.silly("bbox = " + minX + ", " + minY + ", " + maxX + ", " + maxY);
+								metadataInsert = 
+									"insert into " + schemaName + ".metadata (collection_id, type, json_data, metadata_file, geom) values (" +
+									collectionID + ", $$" + 
+									type + "$$, $$" + 
+									JSON.stringify(data) + "$$, $$" +
+									path.join(intermediateDir, myDir, file) + "$$, " +
+									"ST_MakeEnvelope(" + minX + "," + minY + "," + maxX + "," + maxY + ",4326)) returning metadata_id";
+							}
+						}
+						logger.silly(metadataInsert);
+						
 						db.one(metadataInsert).then((data) => {
 							//console.log(file.substring(file.indexOf('-')+1, file.lastIndexOf('.')));
 							idReturn.push({file: file, metadataID: data.metadata_id});
