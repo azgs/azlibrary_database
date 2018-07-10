f9acc1bf11d1cbd2d1fca92f7f58985124ae7752
Douglas Meredith <douglasm@email.arizona.edu>
2018-07-10 16:10:25 -0700

    Added geom column to metadata table

--- azlibConfigPG/schemas/02-metadata.sql
+++ azlibConfigPG/schemas/02-metadata.sql
@@ -14,7 +14,8 @@ CREATE TABLE metadata.metadata
 	collection_id integer REFERENCES public.collections(collection_id) not null, 
 	type text references metadata.types(type_name) not null,
 	json_data jsonb not null,
-	metadata_file text not null
+	metadata_file text not null,
+	geom geometry
 );
 
 
