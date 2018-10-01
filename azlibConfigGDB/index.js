d8c012cf740f69a7ba85bdd70ec4c8d496d1b525
Douglas Meredith <douglasm@email.arizona.edu>
2018-10-01 11:50:21 -0700

    More of those bigly changes. Stop using gdb template and start using sql backup to create schema

--- azlibConfigGDB/index.js
+++ azlibConfigGDB/index.js
@@ -22,19 +22,6 @@ const path = require("path");
 //TODO: Kind of cheesy to have to do this to get path to this folder. Is there a better way?
 const pathToMe = require("global-modules-path").getPath("azlibConfigGDB");
 
-const sourceGDB = path.join(pathToMe, "ncgmp09", "ncgmp09.gdb"); //TODO: Using ncgmp09 gdb for everything for now. Should be ok I think.
-
-const ogr2ogr = require('ogr2ogr');
-
-const gdal = require("gdal");
-const dataset = gdal.open(sourceGDB);
-const layers = dataset.layers.map((layer, i) => {
-	return args.gdbschema + '."' + layer.name + '"';
-});
-//console.log(layers);
-//console.log(layers.toString());
-
-
 // get password sorted
 let promise = new Promise((resolve) => {
 	if (args.length === 4) {
@@ -64,42 +51,8 @@ promise.then((password) => {
 
 	//create schema
 	return db.none('create schema ' + args.gdbschema).catch(error => {throw new Error(error);});
-}).then(() => { 
-	//fill it with tables from a dummy gdb 
-	//TODO: Move the ogr2ogr call to a module that wraps it in a promise
-	const ogrPromise = new Promise((resolve, reject) => {
-		ogr2ogr(sourceGDB) 
-		.format('PostgreSQL')
-		.options(['-lco', 'GEOMETRY_NAME=geom', '-lco', 'LAUNDER=NO', '-gt', 'unlimited'])
-		.destination('PG:host=localhost user=' + args.username + ' password=' + args.password + ' dbname=' + args.dbname + ' schemas=' + args.gdbschema)
-		.exec((error, data) => {
-			if (error) {
-				reject(error);
-			} else {
-				resolve(data);
-			}
-		});
-	});
-	return ogrPromise.catch(error => {throw new Error(error);});
 }).then((data) => {
-	return db.none('set search_path to ' + args.gdbschema).catch(error => {throw new Error(error);}); //TODO: not sure this is necessary
-}).then(() => {
-	//clear out the data (all we wanted was the tables)
-	return db.none('truncate ' + layers.toString() + ' cascade').catch(error => {throw new Error(error);});
-}).then(() => {
-	console.log("Truncate successful");
-
-	//add collection_id column to each table
-	cidPromises = layers.map(layer => {
-		//console.log(layer);
-		return db.none('alter table ' + layer + ' add column collection_id integer references public.collections (collection_id)')
-		.then(() => {
-			console.log("successfully added collection_id to " + layer)
-		})
-		.catch(error => {throw new Error(error);});
-	});  
-
-	return Promise.all(cidPromises).catch(error => {throw new Error(error);});
+	return db.none('set search_path to ' + args.gdbschema + ',public').catch(error => {throw new Error(error);}); //TODO: not sure this is necessary
 }).then(() => {
 	//TODO: Currently using the same template for ncgmp09 and gems. This is not accurate and will change in the future.
 	if (args.gdbschema.toLowerCase() === "ncgmp09" || args.gdbschema.toLowerCase() === "gems") {
@@ -111,7 +64,14 @@ promise.then((password) => {
 			throw new Error(error);
 		});
 	} else {
-		return Promise.resolve();
+		//return Promise.resolve();
+		//TODO: Also using same template for everything else
+		const file = pgp.QueryFile(path.join(pathToMe, 'ncgmp09', 'ncgmp09.sql'), {minify: true});
+
+		return db.none(file).catch(error => {
+			console.log("Problem processing ncgmp09 template: ");console.log(error); 
+			throw new Error(error);
+		});
 	}
 }).then(() => {
 	console.log("collection_id successfully added to all tables");
