f8720fe54cb9afbbaee7a2901d2de2e29ebf6765
Douglas Meredith <douglasm@email.arizona.edu>
2018-10-09 15:24:50 -0700

    Pull in new metadata json query path columns

--- azlibAdd/config.js
+++ azlibAdd/config.js
@@ -25,9 +25,20 @@ exports.load = (db) => {
 			global.rasterTypes = data.map(datum => {return datum.type_name});
 			logger.debug("Raster types = " + global.rasterTypes)
 		}),
-		db.any("select type_name from metadata.types").then(data => {
-			global.metadataTypes = data.map(datum => {return datum.type_name});
-			logger.debug("Metadata types = " + global.metadataTypes)
+		db.any("select * from metadata.types").then(data => {
+			global.metadataTypes = data.map(datum => {
+				return {
+					name: datum.type_name, 
+					formalNamePath: datum.title_query_path,
+					xMinPath: datum.minx_query_path,
+					xMaxPath: datum.maxx_query_path,
+					yMinPath: datum.miny_query_path,
+					yMaxPath: datum.maxy_query_path
+				}
+			});
+			
+			logger.debug("Metadata types = " + global.pp(global.metadataTypes));
+			logger.debug("ISO path = " + metadataTypes[1].formalNamePath);
 		})
 	];
 
