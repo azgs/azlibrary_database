91ca0ba2b7523f91ac481a1dff143cc3db5ae267
Douglas Meredith <douglasm@email.arizona.edu>
2018-10-24 15:03:33 -0700

    Added triggers to set up text search columns

--- azlibConfigPG/schemas/02-metadata.sql
+++ azlibConfigPG/schemas/02-metadata.sql
@@ -246,9 +246,126 @@ CREATE TABLE metadata.metadata
 	collection_id integer REFERENCES public.collections(collection_id) not null, 
 	type text references metadata.types(type_name) not null,
 	json_data jsonb not null,
+	title_search tsvector,
+	author_search tsvector,
+	keyword_search tsvector,
+	series_search tsvector,
+	kitchensink_search tsvector,
 	metadata_file text not null,
 	geom geometry
 );
+
+CREATE FUNCTION metadata.title_trigger() RETURNS trigger AS $$
+begin
+  new.title_search :=
+     to_tsvector('pg_catalog.english', (
+		select 
+			new.json_data->
+				'gmd:MD_Metadata'->
+					'gmd:identificationInfo'->0->
+						'gmd:MD_DataIdentification'->0->
+							'gmd:citation'->0->
+								'gmd:CI_Citation'->0->
+									'gmd:title'->0->
+										'gco:CharacterString'->>0));
+  return new;
+end
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER titleTSVupdate BEFORE INSERT OR UPDATE
+    ON metadata.metadata FOR EACH ROW EXECUTE PROCEDURE metadata.title_trigger();
+
+CREATE FUNCTION metadata.author_trigger() RETURNS trigger AS $$
+begin
+  new.author_search :=
+     to_tsvector('pg_catalog.english', (
+		SELECT  
+			k.authors
+		FROM 
+				(SELECT 
+					string_agg(author, ';') as authors
+				FROM (
+		
+					select jsonb_array_elements_text(
+						jsonb_array_elements(
+							jsonb_array_elements(
+								new.json_data #> 
+								'{"gmd:MD_Metadata", "gmd:identificationInfo"}'					
+							) 
+							#> '{"gmd:MD_DataIdentification", 0, "gmd:citation", 0, "gmd:CI_Citation", 0, "gmd:citedResponsibleParty"}'
+						) 
+						#> '{"gmd:CI_ResponsibleParty", 0, "gmd:individualName", 0, "gco:CharacterString"}'
+					) author
+				) as a
+			) k));
+  return new;
+end
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER authorTSVupdate BEFORE INSERT OR UPDATE
+    ON metadata.metadata FOR EACH ROW EXECUTE PROCEDURE metadata.author_trigger();
+
+CREATE FUNCTION metadata.keyword_trigger() RETURNS trigger AS $$
+begin
+  new.keyword_search :=
+     to_tsvector('pg_catalog.english', (
+		SELECT  
+			k.keywords
+		FROM 
+				(SELECT 
+					string_agg(keyword, ',') as keywords
+				FROM (
+		
+					select jsonb_array_elements_text(
+						jsonb_array_elements(
+							jsonb_array_elements(
+								jsonb_array_elements(
+									new.json_data #> 
+									'{"gmd:MD_Metadata", "gmd:identificationInfo"}'					
+								) 
+								#> '{"gmd:MD_DataIdentification", 0, "gmd:descriptiveKeywords"}'
+							) 
+							#> '{"gmd:MD_Keywords",0, "gmd:keyword"}'
+						) 
+						#>'{"gco:CharacterString"}'
+					) keyword
+				) as a
+			) k));
+  return new;
+end
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER keywordTSVupdate BEFORE INSERT OR UPDATE
+    ON metadata.metadata FOR EACH ROW EXECUTE PROCEDURE metadata.keyword_trigger();
+
+CREATE FUNCTION metadata.series_trigger() RETURNS trigger AS $$
+begin
+  new.series_search :=
+     to_tsvector('pg_catalog.english', (
+		SELECT  
+			k.series
+		FROM 
+				(SELECT 
+					string_agg(serial, ';') as series
+				FROM (
+					select jsonb_array_elements_text(
+						jsonb_array_elements(
+							jsonb_array_elements(
+								new.json_data #> 
+								'{"gmd:MD_Metadata", "gmd:identificationInfo"}'					
+							) 
+							#> '{"gmd:MD_DataIdentification", 0, "gmd:citation", 0, "gmd:CI_Citation", 0, "gmd:identifier"}'
+						) 
+						#> '{"gmd:MD_Identifier", 0, "gmd:code", 0, "gco:CharacterString"}'
+					) serial
+				) as a
+			) k));
+  return new;
+end
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER seriesTSVupdate BEFORE INSERT OR UPDATE
+    ON metadata.metadata FOR EACH ROW EXECUTE PROCEDURE metadata.series_trigger();
 
 
 /*
