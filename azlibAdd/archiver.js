56c27aff9f256b46669d350e2e11d4da9db5050c
Douglas Meredith <douglasm@email.arizona.edu>
2018-11-29 14:10:08 -0700

    Changed to node-tar instead of tar-fs. This fixes the problem in which tar's did not include a collection's top-level dir, only it's content.

--- azlibAdd/archiver.js
+++ azlibAdd/archiver.js
@@ -6,35 +6,21 @@ exports.archive = (sourceDir, destDir) => {
 	logger.silly("sourceDir = " + sourceDir);
 	logger.silly("destDir = " + destDir);
 
-	return new Promise((resolve, reject) => {
-		try {
-			const fs = require('fs-extra');
-			const util = require("util");
-			const path = require("path");
-
-			const zlib = require('zlib');
-			const gzipper = zlib.createGzip();
-
-			fs.ensureDir(destDir).then(() => {
-
-				const gz = fs.createWriteStream(path.join(destDir, path.basename(sourceDir) + ".tar.gz"));
-
-				const tarrer = require("tar-fs");
-				tarrer.pack(sourceDir).pipe(gzipper).pipe(gz); 
-
-				gz.on('finish', () => {
-					fs.remove(sourceDir).then(() => {
-						resolve();
-					}).catch(err => {
-						logger.error("Problem removing directory");
-						logger.error(err);
-						throw new Error(err);
-					});
-				});
-			});
-		} catch(err) {
-			reject(err);
-		}
+	const fs = require('fs-extra');
+	const path = require("path");
+	const tar = require("tar");
+	return fs.ensureDir(destDir).then(() => {
+		return tar.create({
+			file: path.join(destDir, path.basename(sourceDir)) + ".tar.gz",
+			cwd: path.dirname(sourceDir),
+			gzip: true
+		}, [path.basename(sourceDir)]);
+	}).then(() => {
+		return fs.remove(sourceDir);
+	}).catch(error => {
+		logger.error("Problem creating archive: " + global.pp(error));
+		return Promise.reject(error);
 	});
+
 }
 
