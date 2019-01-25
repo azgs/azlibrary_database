1ced72365042ced9a98e94e3a1a1c22fc6f78361
Douglas Meredith <douglasm@email.arizona.edu>
2019-01-25 15:30:41 -0700

    Now just store path to file

--- azlibAdd/metadata.js
+++ azlibAdd/metadata.js
@@ -13,29 +13,24 @@ exports.upload = (rootDir, intermediateDir, schemaName, collectionID, db) => {
 	//Verify that directory exists
 	const fs = require('fs');
 	if (!fs.existsSync(dir)) {
-		if (intermediateDir) {		
-			return Promise.resolve();
-		} else {
-			return Promise.reject("No top level metadata directory found for collection");
-		}
+		return Promise.resolve();
 	}
 
 	const idReturn = [];
 
 	const files = fs.readdirSync(dir).filter(f => !fs.statSync(path.join(dir, f)).isDirectory());
-	//filter out files that don't end in "xml", and files that don't meet naming requirement
- 	const validFiles = files.filter( f => (f.split('.')[f.split('.').length-1].toUpperCase() === "XML" ||
-											f.split('.')[f.split('.').length-1].toUpperCase() === "JSON") &&
-											global.metadataTypes.map(mType => {return mType.name}).includes(f.split('.')[0].split('-')[0].toUpperCase()));
+	//filter out files that don't end in "xml" or "json", and files that don't meet naming requirement
+ 	const validFiles = files.filter( f => ((f.split('.')[f.split('.').length-1].toUpperCase() === "XML" ||
+											f.split('.')[f.split('.').length-1].toUpperCase() === "JSON")) &&
+											["ISO19115", "ISO19139"].includes(f.split('.')[0].split('-')[0].toUpperCase()));
+
+
+
 	logger.silly("files = " + global.pp(files));
 	logger.silly("validFiles = " + global.pp(validFiles));
 
 	if (files.length !== validFiles.length) {
 		logger.warn("Invalid metadata files ignored under " + path.join(intermediateDir, myDir));
-	}
-
-	if (!intermediateDir && validFiles.length === 0) {
-		return Promise.reject("No suitable top level metadata files found for collection");
 	}
 
 	//Process each file
@@ -45,123 +40,23 @@ exports.upload = (rootDir, intermediateDir, schemaName, collectionID, db) => {
 		const fileMetadataType = file.split('.')[0].split('-')[0].toUpperCase();
 
 		//read xml file
-		const xmlPath = path.resolve(dir, file);//process.cwd() + "/" + dir + "/" + file;
+		const xmlPath = path.resolve(dir, file);
 
-		const util = require("util");
+		let metadataInsert = 
+				"insert into " + schemaName + ".metadata (collection_id, metadata_file) values (" +
+				collectionID + ", $$" + 
+				path.join(intermediateDir, myDir, file) + "$$) returning metadata_id";
+		logger.silly("insert for " + file + " = " + metadataInsert);
 
-		const readFilePromise = util.promisify(fs.readFile);
-		return readFilePromise(xmlPath, 'utf-8')
-		.catch(error => {logger.warn("no xml data in " + file + ": " + error);return Promise.reject(error);})
-		.then((data) => {      
-			logger.silly("processing metadata content for " + file);
-			logger.silly("data = " + global.pp(data));
-			logger.silly(path.extname(file).toUpperCase());
-			if (".JSON" === path.extname(file).toUpperCase()) {
-				logger.silly("json file");
-				return JSON.parse(data);
-			} else if (".XML" === path.extname(file).toUpperCase()) {
-				logger.silly("xml file");
-				const xml = require("./xmlConverter");
-				return xml.convert(data, fileMetadataType);
-			} else {
-				//Unknown filetype. Return empty object.
-				return {};
-			}
-
-			//return xml2js(data).catch(error => {logger.warn("Problem parsing xml in " + file + ": " + error); return Promise.reject(error);})
-		}).catch(error => {
-			logger.warn("Problem parsing xml in " + file + ": " + error); 
-			//return Promise.reject(error);
-		}).then((data) => {
-			logger.silly("json from " + file + " = " + global.pp(data));
-
-			//Can only set these in AZGS metadata, but it's possible we have something else here.
-			//Just eat the error.
-			try {
-				data.identifiers.collection_id = collectionID;
-				if (global.args.archive) {
-					data.identifiers.directory = path.resolve(global.args.archive, ""+collectionID);
-				} else {
-					data.identifiers.directory = path.resolve(global.args.source);
-				}
-				logger.silly("json with collection info = " + global.pp(data));
-			} catch (error) {
-				logger.silly("Unable to set collection info");
-			}
-
-			let metadataInsert, collectionsUpdate; 
-
-			//If not top level metadata file					
-			if (intermediateDir) {
-				logger.silly("This has intermediateDir: " + intermediateDir);
-				metadataInsert = 
-					"insert into " + schemaName + ".metadata (collection_id, type, json_data, metadata_file) values (" +
-					collectionID + ", $$" + 
-					type + "$$, $$" + 
-					JSON.stringify(data) + "$$, $$" +
-					path.join(intermediateDir, myDir, file) + "$$) returning metadata_id";
-			} else {
-				//const fileMetadataType = file.split('.')[0].split('-')[0].toUpperCase();
-				if (global.metadataTypes.filter(t => t.formalNamePath).map(t => t.name)
-					.includes(fileMetadataType)) {
-					//const metadataType = global.metadataTypes.filter(t => t.name === fileMetadataType)[0];
-					//At thist point, we know the metadata has been converted to AZGS format.
-					//TODO: Arguably, we don't need to go to the db table here anymore. Could hardcode.					
-					const metadataType = global.metadataTypes.filter(t => t.name === "AZGS")[0];
-
-					try {
-						logger.silly("title path = " + jsonQueryPathToArrayPath(metadataType.formalNamePath));
-						const title = eval("data" + jsonQueryPathToArrayPath(metadataType.formalNamePath));
-						logger.silly("title = " + title);
-
-						collectionsUpdate = "update collections set formal_name = $$" + title + "$$ where collection_id = " + collectionID;
-					} catch (error) {
-						throw new Error("Could not determine formal name for collection from metadata");
-					}
-				
-					try {
-						const minX = eval("data" + jsonQueryPathToArrayPath(metadataType.xMinPath));
-						const maxX = eval("data" + jsonQueryPathToArrayPath(metadataType.xMaxPath));
-						const minY = eval("data" + jsonQueryPathToArrayPath(metadataType.yMinPath));
-						const maxY = eval("data" + jsonQueryPathToArrayPath(metadataType.yMaxPath));
-				
-						logger.silly("bbox = " + minX + ", " + minY + ", " + maxX + ", " + maxY);
-						metadataInsert = 
-							"insert into " + schemaName + ".metadata (collection_id, type, json_data, metadata_file, geom) values (" +
-							collectionID + ", $$" + 
-							fileMetadataType + "$$, $$" + 
-							JSON.stringify(data) + "$$, $$" +
-							path.join(intermediateDir, myDir, file) + "$$, " +
-							"ST_MakeEnvelope(" + minX + "," + minY + "," + maxX + "," + maxY + ",4326)) returning metadata_id";
-					} catch (error) {
-						throw new Error("Could not determine geographic bounding box for collection from metadata: " + global.pp(error));
-					}
-				} else {
-					throw new Error("Unrecognized top-level xml format. Can't create insert statement.");
-				}
-			}
-			logger.silly("insert for " + file + " = " + metadataInsert);
-
-			return db.one(metadataInsert)
-			.catch(error => {
-				logger.error("Problem inserting metadata record: " + global.pp(error));
-				return Promise.reject("Problem inserting metadata record: " + error);
-			})
-			.then((data) => {
-				idReturn.push({file: file, metadataID: data.metadata_id});
-				//Update formal_name in collections from the json metadata
-				if (collectionsUpdate) {
-					logger.silly("Top level metadata, so updating collections");
-					return db.none(collectionsUpdate).catch(error => {
-						logger.error("Problem updating formal_name in collections: " + global.pp(error));
-						return Promise.reject(error);
-					});
-				} else { //not a top level metadata file; don't update collections
-					logger.silly("Not top level metadata, so not updating collections");
-					return Promise.resolve();
-				}
-			})
-		});
+		return db.one(metadataInsert)
+		.catch(error => {
+			logger.error("Problem inserting metadata record: " + global.pp(error));
+			return Promise.reject("Problem inserting metadata record: " + error);
+		})
+		.then((data) => {
+			idReturn.push({file: file, metadataID: data.metadata_id});
+			return Promise.resolve();
+		})
 
 	});
 	return Promise.all(promises)
@@ -169,24 +64,6 @@ exports.upload = (rootDir, intermediateDir, schemaName, collectionID, db) => {
 		logger.error("Problem processing metadata for " + schemaName + ": " + global.pp(error)); 
 		return Promise.reject(error);
 	}).then(() => Promise.resolve(idReturn));
-}
-
-
-function jsonQueryPathToArrayPath(jsonQPath) {
-	if (jsonQPath) {
-		//slice off json_data prefix if present
-		if (jsonQPath.startsWith("json_data->")) {
-			jsonQPath = jsonQPath.slice(11);
-		}
-		if (jsonQPath.startsWith(">")) { //TODO: consolidate with previous
-			jsonQPath = jsonQPath.slice(1);
-		}
-		const regex =/(.+?)(->{1,2}|$)/gm;
-		return jsonQPath.replace(regex, (a, b) => {
-			return '[' + b.trim() + ']';
-		});
-	}
-	throw new Error("JSON query path not defined");
 }
 
 
