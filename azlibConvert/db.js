fcb472477b08b0e70c7bd2f1c608943b4fd77026
Douglas Meredith <douglasm@email.arizona.edu>
2019-02-13 16:29:50 -0700

    Added logic to get collection group info based on azgs_old_url

--- azlibConvert/db.js
+++ azlibConvert/db.js
@@ -14,9 +14,28 @@ exports.fetch = (data) => {
 		link.url = result.ua_library;
 		link.name = "UA Library";
 		data.links.push(link);
-		return Promise.resolve(data);
+		return Promise.resolve();
 	}).catch(error => {
 		logger.warn("Unable to find collections record for " + global.datasetName + ": " + global.pp(error));
+		return Promise.resolve(data);
+	}).then(data => {
+		const query = `
+			select 
+				cg.collection_group_id, 
+				cg.collection_group_name 
+			from
+				collection_groups cg join 
+				initial_group_mapping igm on 
+					cg.collection_group_name = igm.collection_group_name
+			where azgs_old_url like $1
+		`
+		return global.db.one(query, ["%/" + global.datasetName]).then(result => {
+			data.collection_group.name = result.collection_group_name;
+			data.collection_group.id = result.collection_group_id;
+			return Promise.resolve(data);
+		});
+	}).catch(error => {
+		logger.warn("Unable to find collection group information for " + global.datasetName + ": " + global.pp(error));
 		return Promise.resolve(data);
 	});
 
