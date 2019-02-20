171b208322ec63828e5a3efaf63e68ae4eef693e
Douglas Meredith <douglasm@email.arizona.edu>
2019-02-19 17:29:12 -0700

    Removed some ghosted code. Replaced blanks with null in classes

--- azlibAdd/azgsMetadata.js
+++ azlibAdd/azgsMetadata.js
@@ -59,69 +59,44 @@ exports.upload = (metadata, db) => {
 		});
 		*/
 		
-/*
-		return db.tx(t => { 
-			const metadataInsert = 
-				"insert into metadata.azgs (collection_id, json_data, geom) values (" +
-				collectionID + ", $$" + 
-				JSON.stringify(metadata) + "$$, " +
-				"ST_MakeEnvelope(" + metadata.bounding_box.west + "," + 
-					metadata.bounding_box.south + "," + 
-					metadata.bounding_box.east + "," + 
-					metadata.bounding_box.north + ",4326))";
-			logger.silly("insert = " + metadataInsert);
-
-			const collectionsUpdate = "update public.collections set formal_name = $$" + metadata.title + 
-										"$$, informal_name = $$" + metadata.informal_name + 
-										"$$ where collection_id = " + collectionID;
-			logger.silly("update for = " + collectionsUpdate);
-
-			return t.batch([metadataInsert, collectionsUpdate]);
-		})
-		.then(data => {
-			logger.silly("azgs metadata successful");
-		})
-		.catch(error => {logger.error(error);throw new Error(error);});
-*/
-
 	});
 };
 
 exports.Metadata =	class {
 	constructor () {
-		this.title = "";
+		this.title = null;
 		this.authors = [];
 		this.collection_group = {
-			name: "",
-			id: ""
+			name: null,
+			id: null
 		};
-		this.year = "";
+		this.year = null;
 		this.journal = {
-			name: "",
-			publisher: "",
-			url: ""
+			name: null,
+			publisher: null,
+			url: null
 		}
-		this.series = "";
-		this.abstract = "";
+		this.series = null;
+		this.abstract = null;
 		this.links = [];
 		this.identifiers = {
-			collection_id: "",
-			directory: "",
-			doi: ""
+			collection_id: null,
+			directory: null,
+			doi: null
 		};
 		this.files = [];
 		this.language = "English", // prepopulate english
 		this.license = {
-			type: "",
-			url: ""
+			type: "CC BY-NC-SA 4.0",
+			url: "https://creativecommons.org/licenses/by-nc-sa/4.0/"
 		};
 		this.keywords = [];
-		this.informal_name = "";    
+		this.informal_name = null;    
 		this.bounding_box = {
-			north: "",
-			south: "",
-			east: "",
-			west: ""
+			north: null,
+			south: null,
+			east: null,
+			west: null
 		};
 		this.private = "false" //prepopulate false?
 	}
@@ -130,105 +105,33 @@ exports.Metadata =	class {
 
 exports.Author = class {
 	constructor() {	
-		this.person = "";
-		this.organization = "";
-		this.givenname= "";
-		this.surname = "";
+		this.person = null;
+		this.organization = null;
+		this.givenname= null;
+		this.surname = null;
 	}
 };       
 
 exports.Link = class {
 	constructor() {	
-		this.url = "";
-		this.name = "";
+		this.url = null;
+		this.name = null;
 	}
 };
 
 exports.File = class {
 	constructor() {	
-		this.name = "";
-		this.extension = "";
-		this.type = "";
+		this.name = null;
+		this.extension = null;
+		this.type = null;
 	}
 };          
 
 exports.Keyword = class {
 	constructor() {	
-		this.name = "";
-		this.type = "";
+		this.name = null;
+		this.type = null;
 	}
 };
 
-
-
-
-/*
-exports.metadata =	{
-	"title": "", // required
-	"authors": [ 
-		//0..n author        
-	],
-	"collection_group": {
-		"name": "",
-		"id": ""
-	},
-	"year": "", // required 
-	"journal": {
-		"name": "",
-		"publisher": "",
-		"url": ""
-	},
-	"series": "", // required
-	"abstract": "", // required
-	"links": [
-		//0..n link
-	],
-	"identifiers": {
-		"collection_id": "",
-		"directory": "", // required
-		"doi": ""
-	},
-	"files": [ 
-		//0..n file          
-	],
-	"language": "English", // prepopulate english
-	"license": {
-		"type": "", // required
-		"url": "" // required
-	},
-	"keywords": [
-		//0..n keyword
-	],
-	"informal_name": "", // required for NCGMP09 maps   
-	"bounding_box": {
-		"north": "", // required
-		"south": "", // required
-		"east": "", // required
-		"west": "" // required
-	},
-	"private": "false" //prepopulate false?
-};
-
-exports.author = {
-	"person": "", // required
-	"organization": "",
-	"givenname": "",
-	"surname": ""
-};       
-
-exports.link = {
-	"url": "",
-	"name": ""
-};
-
-exports.file = {
-	"name": "", // required
-	"type": "" // required
-};          
-
-exports.keyword = {
-	"name": "",
-	"type": ""
-};
-*/
 
