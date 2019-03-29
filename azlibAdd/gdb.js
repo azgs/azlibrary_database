602c4040bc22fea903528daba3e472b874d63561
Douglas Meredith <douglasm@email.arizona.edu>
2019-03-29 15:21:17 -0700

    This module ended up needing a lot of attention due to the change to using a transaction throughout. ogr2ogr can't use that transaction. It opens its own connection. This led to grief with rollback and unpredictable weirdness. I ended up completely overhauling this module to load the gdb into a temporary schema first, then copy it over to the real schema. There were a number of iterations along the way, and yes, I should have created a branch for this work in order to track these. Whatever, here it all is.

--- azlibAdd/gdb.js
+++ azlibAdd/gdb.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const logger = require("./logger")(path.basename(__filename));
 
-exports.upload = (dir, schemaName, collectionID, db) => {
+exports.upload = (dir, schemaName, collection, db) => {
 	logger.debug("enter with schemaName = " + schemaName);
 
 	//First, verify that schema exists
-	return db.one("select schema_name from information_schema.schemata where schema_name = '" + schemaName + "';")
+	return db.one(`
+		select 
+			schema_name 
+		from 
+			information_schema.schemata 
+		where 
+			schema_name = '${schemaName}'
+	`)
 	.catch(error => {console.log("Schema " + schemaName + " does not exist.");return Promise.reject(error);})
 	.then(() => {
 
@@ -33,9 +40,14 @@ exports.upload = (dir, schemaName, collectionID, db) => {
 			return layer.name;
 		});
 
+		//ogr2ogr works outside of the current db transaction. This causes problems in the event of
+		//and error and db rollback. So, we let it upload into a temporary schema. Then we copy 
+		//the content of that schema using the transaction.
+		const tmpSchema = collection.permID.replace(/-/g, '_').toLowerCase();
+
 		//Fetch list of existing tables. These are the only layers we will want from the gdb.
 		//return db.any("select table_name from information_schema.tables where table_schema='" +  schemaName + "'")	
-		return db.any("select name, required from " + schemaName + ".layers")	
+		return db.any(`select name, required from ${schemaName}.layers`)	
 		.then(tables => {
 
 			//create list of required layers
@@ -65,50 +77,117 @@ exports.upload = (dir, schemaName, collectionID, db) => {
 			}, []);
 			if (missingRequiredLayers.length > 0) return Promise.reject("GDB is missing required layers: " + missingRequiredLayers);
 
-			//create list of layers with schema name
-			const qualifiedLayers = layers.map((layer) => {
-				return schemaName + '."' + layer + '"';
-			});
-
 			const ogr2ogr = require('ogr2ogr');
 
-			//append new gdb to existing gdb (fetching only the layers we want)
-			const ogrPromise = new Promise((resolve, reject) => {
-				ogr2ogr(dir + "/" + dirs[0])
-				.format('PostgreSQL')
-				.project('EPSG:4326')
-				.timeout(50000)
-				.options(layers.concat(['-lco', 'GEOMETRY_NAME=geom', '-lco', 'LAUNDER=NO', '-append']))
-				.destination('PG:host=localhost user=' + args.username + ' password=' + args.password + ' dbname=' + args.dbname + ' schemas=' + schemaName)
-				.exec(function(error, data) {
-					if (error) {
-						reject(error);
-					} else {
-						resolve(qualifiedLayers);
-					}
+			const util = require('util');
+			const exec = util.promisify(require('child_process').exec);
+			
+			//azlibConfigGDB creates the temporary schema. It also works outside of the overall db
+			//transaction. We pass a -c option here to tell it to create the schema but not load it.
+			//This allows us to trap possible schema errors in the incoming collection. Prior to this,
+			//ogr2ogr just kinda rolled with it.
+			const execStr = 'azlibConfigGDB -c -g ' + tmpSchema + ' -d achoo -u ' + args.username + ' -p ' + args.password;
+			logger.silly(execStr);
+			return exec(execStr)
+			.then(() => {
+				logger.silly("in ogr then");
+				const ogrPromise = new Promise((resolve, reject) => {
+					ogr2ogr(dir + "/" + dirs[0])
+					.format('PostgreSQL')
+					.project('EPSG:4326')
+					.timeout(50000)
+					.options(layers.concat(['-lco', 'GEOMETRY_NAME=geom', '-lco', 'LAUNDER=NO']))
+					.destination('PG:host=localhost user=' + args.username + ' password=' + args.password + ' dbname=' + args.dbname + ' schemas=' + tmpSchema)
+					.exec(function(error, data) {
+						if (error) {
+							logger.silly("ogr fail");
+							reject(error);
+						} else {
+							logger.silly("ogr success");
+							resolve(layers);
+						}
+					});
 				});
+				return ogrPromise.catch(error => {logger.error(error);throw new Error(error);});
+			}).catch((error) => {
+				logger.error("Failed to create and populate temp schema: " + global.pp(error));
+				return Promise.reject(error);
 			});
-			return ogrPromise.catch(error => {logger.error(error);throw new Error(error);});
 
 		}).then ((layers) => {
-			//Set collection_id in each record that has a null
-			//TODO: This approach is a little janky
+			logger.silly("In post-ogr then, layers = " + global.pp(layers));
+			//Add collection_id column to each table, set to the current collectionID.
 			const cidPromises = layers.map((layer) => {
-				//console.log("layer = " + layer);
-				return db.none("update " + layer + 
-					" set collection_id = " + collectionID +
-					" where collection_id is null")
+				return db.none(`					
+					alter table ${tmpSchema}."${layer}"
+					add column collection_id integer not null default ${collection.collectionID}
+				`)
 				.then(() => {
-					logger.debug("successfully updated collection_id for " + layer);
+					logger.debug("successfully added collection_id for " + layer);
 				})
 				.catch(error => {throw new Error(error);});
 			});
-			return Promise.all(cidPromises).catch(error => {throw new Error(error);});
+			return Promise.all(cidPromises).catch(error => {throw new Error(error);}).then(() => {return Promise.resolve(layers)});
+		}).then ((layers) => {
+			logger.silly("In copy section, layers = " + global.pp(layers));
+			
+			const copyPromises = layers.map((layer) => {
+				logger.silly("copying " + layer);
+
+				//Get list of column names in destination schema. This is a bit of postgres-fu.
+				return db.any(`
+					SELECT 
+						c.column_name
+					FROM 
+						information_schema.columns AS c
+					WHERE 
+						table_schema = '${schemaName}' AND 
+						table_name = '${layer}' AND  
+						c.column_name NOT IN('OBJECTID')
+				`)
+				.catch((error) => {logger.error(global.pp(error)); throw error;})
+				.then((result) => {
+					logger.silly("column query result = " + global.pp(result));
+
+					//Build a string from the columns
+					const columns = result.reduce((acc, column, idx) => {
+						if (idx === result.length-1) {
+							return acc + '"' + column.column_name + '"';
+						} else {
+							return acc + '"' + column.column_name + '",';
+						}
+					},'');
+					logger.silly("columns = " + global.pp(columns));
+
+					//Then use that string to copy the columns we need (all but OBJECTID) into the destination table
+					//Note: This will blow up if there is a mismatch between the source and destination columns.
+					//That's ok.
+					return db.none(`
+						insert into ${schemaName}."${layer}"
+							(${columns})
+							select ${columns} from ${tmpSchema}."${layer}"
+					`).catch(error => {
+						logger.error("Aw man, can't copy. Error: " + global.pp(error)); 
+						return Promise.reject(error);
+					});
+				});
+			});
+			return Promise.all(copyPromises)
+			.catch(error => {
+				logger.error("Problem copying schema: " + global.pp(error));
+				return Promise.reject(error);
+			}).then(() => {return Promise.resolve()});
+		}).then ((layers) => {
+			logger.silly("In drop section");
+			return db.none(`drop schema if exists "${tmpSchema}" cascade`);
+			//return Promise.resolve();//for testing
 		});
 
-	}).catch(error => {logger.error("Problem processing gdb: " + global.pp(error));return Promise.reject(error);})
-	.then(() => {
-		return require("./metadata").upload(dir, "gisdata", collectionID, db);
+	}).catch(error => {
+		logger.error("Problem processing gdb: " + global.pp(error));
+		return Promise.reject(error);
+	}).then(() => {
+		return require("./metadata").upload(dir, "gisdata", collection.collectionID, db);
 	});
 
 };
