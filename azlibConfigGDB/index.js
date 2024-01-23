7963a4e7b1cf916722d421d248277808ccef9654
EC2 Default User <ec2-user@azlibraryapi.colo-prod-aws.arizona.edu>
2024-01-23 16:31:35 -0700

    add instrumentation; add loglevel, host, port, and cert to command line arguments; change from connect string to connect object in setting up pgp connection

--- azlibConfigGDB/index.js
+++ azlibConfigGDB/index.js
@@ -1,24 +1,48 @@
 #!/usr/bin/env node
 
+const path = require("path");
+const fs = require('fs-extra');
+
+global.pp = (object) => {
+	return require('util').inspect(object, {depth:null, maxArrayLength: null});
+};
+
 
 global.args = require('commander');
 
-args
+global.args
 	.version('0.0.1')
 	//.option('-s, --source <source>', 'Source directory of the gdb. Required')
+	.option('-l, --loglevel <loglevel>', 'Indicates logging level (error, warn, info, verbose, debug, silly). Default is info.', 'info')
 	.option('-g, --gdbschema <gdb-schema>', 'Geodatabase schema in DB. If a recognized schema name (e.g. ncgmp09, gems), schema will be prepped accordingly. Required')
 	.option('-d, --dbname <dbname>', 'DB name. Required')
 	.option('-u, --username <username>', 'DB username. Required')
 	.option('-p, --password <password>', 'DB password (will be prompted if not included)')
+        .option('-h, --host <host>', 'Postgres host')
+        .option('-o, --port <port>', 'Postgres port')
+        .option('-s, --cert <cert>', 'SSL public cert for PG')
 	.option('-c, --create ', 'Create schema but do not load it (used for temporary schemas when importing collections)')
 	.parse(process.argv);
 
+const logger = require("./logger")(path.basename(__filename));
+
+//This routine is to facilitate db debugging. It is not necessary.
+const initOptions = {
+    error: function (error, e) {
+        if (e.cn) {
+            // A connection-related error;
+            //logger.debug("CN:", e.cn);
+            logger.debug("EVENT:", error.message);
+        }
+    }
+};
+const pgp = require("pg-promise")(initOptions);
+/*
 const pgp = require("pg-promise")({
 	// Initialization Options
 });
+*/
 let db;
-
-const path = require("path");
 
 //TODO: Kind of cheesy to have to do this to get path to this folder. Is there a better way?
 const pathToMe = require("global-modules-path").getPath("azlibConfigGDB");
@@ -45,30 +69,52 @@ let promise = new Promise((resolve) => {
 
 promise.then((password) => {
 	args.password = password;
-	//console.log("args.password = " + argspassword);
+	logger.debug("password resolved");
 
-	const cn = 'postgres://' + args.username + ':' + args.password + '@localhost:5432/' + args.dbname;
+        let cn = {
+                user: global.args.username,
+                password: global.args.password,
+                host: global.args.host,
+                database: global.args.dbname,
+                port: global.args.port,
+        };
+
+        if (global.args.cert) {
+                cn.ssl = {
+                        rejectUnauthorized: true,
+                        ca: fs.readFileSync(global.args.cert).toString(),
+                };
+        }
+       	logger.silly(global.pp(cn));
+
 	db = pgp(cn);
 
-	return db.tx(t => {
+	return db.tx((t) => {
+		logger.debug("creating schema " + global.args.gdbschema);
 
 		//create schema
-		return t.none('create schema ' + args.gdbschema).catch(error => {throw new Error(error);})
+		return t.none('create schema ' + global.args.gdbschema)
+		.catch(error => {
+			logger.error("schema creation failed");
+			throw new Error(error);})
 		.then(() => {
-			if (args.create) {
+			logger.debug(`empty schema ${args.gdbschema}  created`);
+			if (global.args.create) {
+				logger.silly("create schema only, we're done");
 				return Promise.resolve(); //We're done in this case
 			}
+			logger.silly("fleshing out schema");
 
 			//TODO: The gems sql file is a placeholder. Real one is in development.
-			const schemaFile = args.gdbschema.toLowerCase() === "ncgmp09" ?
+			const schemaFile = global.args.gdbschema.toLowerCase() === "ncgmp09" ?
 								"ncgmp09.sql" :
-								args.gdbschema.toLowerCase() === "gems2" ?
+								global.args.gdbschema.toLowerCase() === "gems2" ?
 									"gems2.sql" :
 									null;
 			if (schemaFile) {
 				const pgpFile = pgp.QueryFile(path.join(pathToMe, 'schemas', schemaFile), {minify: true, params: args.gdbschema});
 				return t.none(pgpFile).catch(error => {
-					console.log("Problem processing gdb template: ");console.log(error); 
+					logger.error("Problem processing gdb template: ");console.log(error); 
 					throw new Error(error);
 				});
 			} else {
@@ -77,17 +123,14 @@ promise.then((password) => {
 		});
 	});
 }).then(() => {
-	console.log("Schema created");
+	logger.debug("Schema created");
 	pgp.end();
 })
 .catch(error => { 
-	console.log(error);
+	logger.error(error);
 	pgp.end();
 	process.exit(1);
 });
-
-	
-			
 
 
 
