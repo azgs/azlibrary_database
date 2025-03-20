85ba1494e10358c2f7e5f85fd29c88c9dcae91f7
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-03-19 19:04:26 -0700

    Formalize DGM formats

---
+++ azlibConfigPG/schemas/patches/14.sql
@@ -0,0 +1,104 @@
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-233"')
+where
+	collection_id = 18861;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-236"')
+where
+	collection_id = 18862;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-143"')
+where
+	collection_id = 17182;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-230"')
+where
+	collection_id = 18740;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-141"')
+where
+	collection_id = 17183;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-142"')
+where
+	collection_id = 17184;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-221"')
+where
+	collection_id = 18741;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-137"')
+where
+	collection_id = 17186;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-143"')
+where
+	collection_id = 17189;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-140"')
+where
+	collection_id = 17185;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-126"')
+where
+	collection_id = 16708;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-139"')
+where
+	collection_id = 17188;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-138"')
+where
+	collection_id = 17187;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-223"')
+where
+	collection_id = 17679;
+
+UPDATE 
+	metadata.azgs
+SET 
+	json_data = jsonb_set(json_data, '{series}', '"DGM-225"')
+where
+	collection_id = 17681;
