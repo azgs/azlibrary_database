d58eb847aa6f332a78f2da95e7cb97d478007a4e
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-03-31 16:40:54 -0700

    Add dicts.mine_collections

---
+++ azlibConfigPG/schemas/patches/15.sql
@@ -0,0 +1,32 @@
+create table if not exists dicts.mine_collections (
+    id serial PRIMARY KEY,
+    name text
+);
+
+insert into dicts.mine_collections (name) values 
+    ('ADMMR'),
+    ('Walter E. Heinrichs, Jr.'), 
+    ('Grover Heinrichs'),
+    ('James Doyle Sell'),
+    ('A. F. Budge mining Ltd.'),
+    ('Doug K. Martin'),
+    ('Cambior Exploration USA'),
+    ('G. M. Colvocoresses'),
+    ('JABA, Inc. Tombstone mining records'),
+    ('Reconstruction Finance Corporation'),
+    ('West Oatman Project'),
+    ('Anderson Mine'),
+    ('Richard Mieritz mining collection'),
+    ('Richinbar Mine photo collection'),
+    ('John E. Kinnison mining collection'),
+    ('Edwin Noel Pennebaker'),
+    ('Kelsey Boltz'),
+    ('Arimetco Inc.'),
+    ('Roland Mulchay'),
+    ('Osborn photo collection'),
+    ('James Doyle Sell mining collection'),
+    ('Guy Atlee photo collection'),
+    ('W. H. Crutchfield, Jr.'),
+    ('Fred Hohne'),
+    ('John E. Kinnison'),
+    ('Larry Kersey');
